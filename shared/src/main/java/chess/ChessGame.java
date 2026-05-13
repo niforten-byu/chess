9fb98a3e8425def031770dcf718bc516029ec383
Duncan <niforten@gmail.com>
2026-05-13 01:19:58 -0600

    implementing phase 1: created hashCode and equals methods for ChessGame.java

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * A class that can manage a chess game, making moves on a board
@@ -102,6 +103,20 @@ public class ChessGame {
      */
     public void setBoard(ChessBoard board) {
         gameBoard = board;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return Objects.equals(gameBoard, chessGame.gameBoard) && currentTurnColor == chessGame.currentTurnColor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameBoard, currentTurnColor);
     }
 
     /**
