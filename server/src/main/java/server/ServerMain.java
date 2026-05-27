97fc0b669e5d38de35f1d083466efa39e281d56f
Duncan <niforten@gmail.com>
2026-05-26 23:22:02 -0600

    implementing phase 4: added tester code to verify sql connection to database

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,7 +1,18 @@
 package server;
 
+import dataaccess.DatabaseManager;
+
 public class ServerMain {
     public static void main(String[] args) {
+
+        // sql connection test
+        try {
+            DatabaseManager.createDatabase();
+            System.out.println("successful database connection ");
+        } catch (Exception e) {
+            System.err.println("database connection failure: " + e.getMessage());
+        }
+
         Server server = new Server();
         server.run(8080);
 
