8ca779a58fad69cb8626ed38cacb117b19bb8210
Duncan <niforten@gmail.com>
2026-06-03 09:54:19 -0600

    implementing phase 5: started wrote positive and negative tests for register, login and logout

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -34,8 +34,54 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerPositive() throws Exception {
+        AuthData authenticationData = facade.register(new UserData("Sung Jinwoo", "thePlayer", "weakestHunter@gmail.com"));
+        Assertions.assertNotNull(authenticationData.authToken());
+        Assertions.assertEquals("Sung Jinwoo", authenticationData.username());
+    }
+
+    @Test
+    public void registerNegative() throws Exception {
+        // register successfully
+        facade.register(new UserData("Cha Hae-In", "theDancer", "everybodySmells@email.com"));
+
+        // attempt to register the same user (this should throw an exception: 403 Already Taken)
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.register(new UserData("Cha Hae-In", "theDancer", "everybodySmells@email.com"));
+        });
+    }
+
+    @Test
+    public void loginPositive() throws Exception {
+        facade.register(new UserData("Sung Il-Hwan", "padre", "badFather@email.com"));
+
+        AuthData authenticationData = facade.login(new LoginRequest("Sung Il-Hwan", "padre"));
+        Assertions.assertNotNull(authenticationData.authToken());
+    }
+
+    @Test
+    public void loginNegative() throws Exception {
+        facade.register(new UserData("Go Gunhee", "oldMan", "whatDoesThisGuyDO@email.com"));
+
+        // attempt login with and password (throw an exception: 401 Unauthorized)
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.login(new LoginRequest("Go Gunhee", "Incorrect_Password"));
+        });
+    }
+
+    @Test
+    public void logoutPositive() throws Exception {
+        AuthData authenticationData = facade.register(new UserData("Goto Ryuji", "japanNumberOne", "iHopeIDontDie@email.com"));
+
+        Assertions.assertDoesNotThrow(() -> facade.logout(authenticationData.authToken()));
+    }
+
+    @Test
+    public void logoutNegative() throws Exception {
+        // try to log out with a fake token (401 Unauthorized)
+        Assertions.assertThrows(ResponseException.class, () -> {
+            facade.logout("non-existent-token");
+        });
     }
 
 }
