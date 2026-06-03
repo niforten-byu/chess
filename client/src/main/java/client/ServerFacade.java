e36773b4fa399531169b90c6eb588e5630de9161
Duncan <niforten@gmail.com>
2026-06-03 02:39:56 -0600

    implementing phase 5: updated ServerFacade and implemented game and clear endpoints

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import model.AuthData;
 import model.UserData;
 import model.LoginRequest;
+import model.CreateGameRequest;
+import model.JoinGameRequest;
+import model.GameData;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -54,7 +57,57 @@ public class ServerFacade {
         this.makeRequest("DELETE", path, null, null, authenticationToken);
     }
 
+    /**
+     * Calls server to clear database (used for tests)
+     */
+    public void clear() throws ResponseException {
+        var path = "/db";
+        // DELETE request to /db
+        this.makeRequest("DELETE", path, null, null, null);
+    }
 
+    /**
+     * Calls server to make new chess game
+     * @param request CreateGameRequest containing new game name
+     * @param authenticationToken Authentication token of user
+     * @return unique gameID made by database
+     */
+    public int createGame(CreateGameRequest request, String authenticationToken) throws ResponseException {
+        var path = "/game";
+
+        // local record to catch JSON response: {"gameID": 1234}
+        record CreateGameResponse(int gameID) {}
+
+        CreateGameResponse response = this.makeRequest("POST", path, request, CreateGameResponse.class, authenticationToken);
+        return response.gameID();
+    }
+
+    /**
+     * Gets all active games from server
+     * @param authenticationToken Authentication token of user
+     * @return Array of GameData records for all games in database asscociated with user
+     */
+    public GameData[] listGames(String authenticationToken) throws ResponseException {
+        var path = "/game";
+
+        // local record to catch JSON {"games": [...]}
+        record ListGamesResponse(GameData[] games) {}
+
+        ListGamesResponse response = this.makeRequest("GET", path, null, ListGamesResponse.class, authenticationToken);
+        return response.games();
+    }
+
+    /**
+     * Join existing game as specific color
+     * @param request             JoinGameRequest with color and gameID
+     * @param authenticationToken Authentication token of user
+     */
+    public void joinGame(JoinGameRequest request, String authenticationToken) throws ResponseException {
+        var path = "/game";
+
+        // PUT request update game in database with user and color
+        this.makeRequest("PUT", path, request, null, authenticationToken);
+    }
 
     /**
      * Make HTTP requests to the server
@@ -116,7 +169,5 @@ public class ServerFacade {
             throw new ResponseException(500, "Error: Failure to connect to server. " + e.getMessage());
         }
     }
-
-
 
 }
